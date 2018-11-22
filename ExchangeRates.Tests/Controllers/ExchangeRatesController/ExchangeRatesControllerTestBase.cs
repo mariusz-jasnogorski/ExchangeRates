cafc65507e31086163f9f7c78f6fbeafa3f00c6f
Mariusz Jasnogórski <mjasnogorski@gmail..com>
2018-11-22 18:12:07 +0100

    Initial commit

---
+++ ExchangeRates.Tests/Controllers/ExchangeRatesController/ExchangeRatesControllerTestBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExchangeRates.Tests.Controllers.ExchangeRatesController
+{
+    public class ExchangeRatesControllerTestBase
+    {
+    }
+}
---
+++ ExchangeRates.Tests/Services/ExchangeRatesService/ExchangeRatesServiceTestBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExchangeRates.Tests.Services.ExchangeRatesService
+{
+    public class ExchangeRatesServiceTestBase
+    {
+    }
+}
